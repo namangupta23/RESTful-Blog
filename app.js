abb472bdf6952e6c0bcba2cda22651fbff56d03e
Naman Gupta <namang133@gmail.com>
2020-03-20 18:56:14 +0530

    Shift database to cloud

--- app.js
+++ app.js
@@ -1,106 +1,100 @@
-var express=require("express");
-var app=express();
-var bodyParser=require("body-parser");
-var mongoose=require("mongoose");
-var methodOverride=require("method-override");
-var expressSanitizer=require("express-sanitizer");
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var methodOverride = require("method-override");
+var expressSanitizer = require("express-sanitizer");
 
-mongoose.connect("mongodb://localhost:27017/restful_blog_app",{useNewUrlParser:true});
-app.use(bodyParser.urlencoded({extended: true}));
+mongoose.connect("mongodb+srv://admin:qwertyuiop@cluster0-d5bry.mongodb.net/rest?retryWrites=true&w=majority", { useNewUrlParser: true });
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressSanitizer());
 app.use(express.static("public"));
 app.use(methodOverride("_method"));
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 
-var blogSchema=new mongoose.Schema({
+var blogSchema = new mongoose.Schema({
     title: String,
     image: String,
     body: String,
-    created: {type: Date, default: Date.now}
+    created: { type: Date, default: Date.now }
 });
 
-var Blog=mongoose.model("Blog",blogSchema);
+var Blog = mongoose.model("Blog", blogSchema);
 
-app.get("/",function(req,res){
+app.get("/", function(req, res) {
     res.redirect("/blogs");
 });
-app.get("/blogs",function(req,res){
-    Blog.find({},function(err,blogs){
-        if(err){
+app.get("/blogs", function(req, res) {
+    Blog.find({}, function(err, blogs) {
+        if (err) {
             console.log("Error!!!");
-        }
-        else{
-            res.render("index",{blogs:blogs});
+        } else {
+            res.render("index", { blogs: blogs });
         }
     })
 });
 
 
-app.get("/blogs/new",function(req,res){
+app.get("/blogs/new", function(req, res) {
     res.render("new");
 });
 
-app.post("/blogs",function(req,res){
-    req.body.blog.body=req.sanitize(req.body.blog.body);
-    Blog.create(req.body.blog,function(err,newBlog){
-        if(err){
+app.post("/blogs", function(req, res) {
+    req.body.blog.body = req.sanitize(req.body.blog.body);
+    Blog.create(req.body.blog, function(err, newBlog) {
+        if (err) {
             res.render("new");
-        }
-        else{
+        } else {
             res.redirect("/blogs");
         }
     })
 })
 
 
-app.get("/blogs/:id",function(req,res){
-    Blog.findById(req.params.id,function(err,foundBlog){
-        if(err){
+app.get("/blogs/:id", function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog) {
+        if (err) {
             res.redirect("/blogs");
-        }
-        else{
-            res.render("show",{blog: foundBlog});
+        } else {
+            res.render("show", { blog: foundBlog });
         }
     });
 });
 
 
-app.get("/blogs/:id/edit",function(req,res){
-    Blog.findById(req.params.id,function(err,foundBlog){
-        if(err){
+app.get("/blogs/:id/edit", function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog) {
+        if (err) {
             res.redirect("/blogs");
-        }
-        else{
-            res.render("edit",{blog: foundBlog});    
+        } else {
+            res.render("edit", { blog: foundBlog });
         }
     });
 });
 
 
-app.put("/blogs/:id",function(req,res){
-    req.body.blog.body=req.sanitize(req.body.blog.body);
-    Blog.findByIdAndUpdate(req.params.id,req.body.blog,function(err,updatedBlog){
-        if(err){
+app.put("/blogs/:id", function(req, res) {
+    req.body.blog.body = req.sanitize(req.body.blog.body);
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog) {
+        if (err) {
             res.redirect("/blogs");
-        }
-        else{
-            res.redirect("/blogs/"+req.params.id);
+        } else {
+            res.redirect("/blogs/" + req.params.id);
         }
     });
 });
 
 
-app.delete("/blogs/:id",function(req,res){
-    Blog.findByIdAndRemove(req.params.id,function(err){
-        if(err){
+app.delete("/blogs/:id", function(req, res) {
+    Blog.findByIdAndRemove(req.params.id, function(err) {
+        if (err) {
             res.redirect("/blogs");
-        }
-        else{
+        } else {
             res.redirect("/blogs");
         }
     });
 });
 
-app.listen("3000",function(){
+app.listen("3000", function() {
     console.log("Server has started on the Local Machine");
 });
